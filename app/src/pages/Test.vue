c37ae99d635b90ee0f11a38d98727dadb06c1a16
taniho_0707 <taniho_0707@yahoo.co.jp>
2021-03-21 20:38:49 +0900

    入れ忘れていたファイルを追加

---
+++ app/src/pages/Test.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="test">
+    <h1>Test Page</h1>
+    <v-btn large color="primary">テスト</v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Test extends Vue {}
+</script>
---
+++ app/src/router/index.ts
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import Test from '../pages/Test.vue'
+
+Vue.use(VueRouter)
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'Test',
+    component: Test
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
